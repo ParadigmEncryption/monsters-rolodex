4a578a94fadafde0f47323ca8b73b23c758e5358
Paradigm <ParadigmEncryption@gmail.com>
2019-07-05 01:11:02 -0500

    Updates

---
+++ precache-manifest.15a4d098079e8d520ccbfdfd3540ecad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ca70a09d6d7b8a57bdeace6dc4d280e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3be4fb22d78c6dee2138",
+    "url": "/monsters-rolodex/static/css/main.2ffe7bcf.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3be4fb22d78c6dee2138",
+    "url": "/monsters-rolodex/static/js/main.2eb5f77f.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
